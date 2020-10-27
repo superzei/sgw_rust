7c6b81dc97ae85e2e982d7b7ba945333a3d32d80
Batuhan ÜNDAR <batuhan.undar@ulakhaberlesme.com.tr>
2020-10-27 18:05:05 +0300

    feat: Multi-tunnel support through configuration file

--- src/gtp_v1.rs
+++ src/gtp_v1.rs
@@ -2,13 +2,15 @@ use std::borrow::Borrow;
 
 pub struct GtpV1 {
     length: u16,
+    teid: u32,
     data: Vec<u8>
 }
 
 impl GtpV1 {
-    pub fn init(data: Vec<u8>) -> GtpV1 {
+    pub fn init(data: Vec<u8>, teid: u32) -> GtpV1 {
         let p = GtpV1 {
             length: data.len() as u16,
+            teid,
             data
         };
 
@@ -19,9 +21,14 @@ impl GtpV1 {
         // split header from data
         let (_header, _data) = data.split_at(8);
         let data = _data.to_vec();
+        let teid: u32 = (_header[4] as u32) << 24 |
+         (_header[5] as u32) << 16 |
+          (_header[6] as u32) << 8 |
+           (_header[7] as u32) << 0;
 
         let p = GtpV1 {
             length: data.len() as u16,
+            teid,
             data
         };
 
@@ -30,6 +37,10 @@ impl GtpV1 {
 
     pub fn get_data(self: &GtpV1) -> &[u8] {
         self.data.as_ref()
+    }
+
+    pub fn get_teid(self: &GtpV1) -> u32 {
+        self.teid
     }
 
     pub fn serialize(self: &mut GtpV1) -> Vec<u8> {
@@ -43,10 +54,10 @@ impl GtpV1 {
         let length = [(self.length >> 8) as u8, self.length as u8];
         header[2] = length[0];  // length, lower bytes
         header[3] = length[1];  // length, upper bytes
-        header[4] = 0x00;  // teid byte 1
-        header[5] = 0x00;  // teid byte 2
-        header[6] = 0x00;  // teid byte 3
-        header[7] = 0x01;  // teid byte 4
+        header[4] = (self.teid >> 24) as u8;  // teid byte 1
+        header[5] = (self.teid >> 16) as u8;  // teid byte 2
+        header[6] = (self.teid >> 8) as u8;  // teid byte 3
+        header[7] = (self.teid >> 0) as u8;  // teid byte 4
 
         let packet: Vec<u8> = [header.as_ref(), data].concat();
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,59 @@
-use std::net;
+extern crate serde;
+
+use serde::{Serialize, Deserialize};
+use std::{io::BufReader, thread::JoinHandle, net};
 use std::env;
-use std::net::{SocketAddr, UdpSocket};
-use std::thread;
+use std::net::{SocketAddr, IpAddr};
 use std::panic;
 use std::process;
+use std::fs;
+use std::collections::HashMap;
 
 use crate::gtp_v1::GtpV1;
-use std::thread::Thread;
+use std::thread;
 
 mod gtp_v1;
+
+struct UdpReceiverPayload {
+    teid: u32,
+    send_socket: net::UdpSocket,
+    receive_address: SocketAddr
+}
+
+struct GtpReceiverPayload {
+    teid_map: HashMap<u32, SenderReceiverPortPair>,
+    receive_address: SocketAddr
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct HostPortPair {
+    host: String,
+    port: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ReceiverSenderPair {
+    receiver: HostPortPair,
+    sender: HostPortPair
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TeidConnectionPair {
+    teid: u32,
+    connection: ReceiverSenderPair
+}
+
+/// Config object
+#[derive(Serialize, Deserialize, Debug)]
+struct ConfigRoot {
+    gtp: ReceiverSenderPair,
+    tunnels: Vec<TeidConnectionPair>
+}
+
+struct SenderReceiverPortPair {
+    teid: u32,
+    sender: SocketAddr
+}
 
 /// Exit whole process on thread panic
 ///
@@ -20,14 +65,42 @@ fn set_exit_hook() {
     }));
 }
 
-/// UDP receiver thread
-///
-fn receiver_thread(address: SocketAddr, callback: &dyn Fn(&[u8], &UdpSocket) -> (), send_address: SocketAddr) {
-    let receiver_socket = net::UdpSocket::bind(address).expect("Unable to bind to udp receive socket!");
+fn gtp_receiver_thread(payload: GtpReceiverPayload) {
+    let receiver_socket = net::UdpSocket::bind(payload.receive_address).expect("Unable to bind to udp receive socket!");
     let sender_socket = net::UdpSocket::bind("0.0.0.0:0").expect("Unable to bind to sender socket");
-    sender_socket.connect(send_address).expect("unable to connect to send address");
 
-    println!("Thread started!");
+    println!("GTP Receiver thread started for: {}:{}", payload.receive_address.ip(), payload.receive_address.port());
+    
+    loop {
+        // receive loop
+        let mut buf = [0; 2000];
+        let (amt, _src) = match receiver_socket.recv_from(&mut buf) {
+            Ok(res) => {res},
+            Err(_e) => {continue}
+        };
+
+        let buf = &mut buf[..amt];
+
+        // data receiver, handle
+        let packet = GtpV1::from_gtp(buf);
+        let addr = match payload.teid_map.get(&packet.get_teid()) {
+            Some(a) => {a},
+            None => {println!("Received message from undefined tunnel. Teid: {}", packet.get_teid()); continue;},
+        };
+
+        match sender_socket.send_to(packet.get_data(), addr.sender) {
+            Ok(_) => {},
+            Err(e) => {println!("Unable to send gtp data to udp socket: {:?}", e)},
+        };
+        
+    }
+
+}
+
+fn udp_receiver_thread(payload: UdpReceiverPayload) {
+    let receiver_socket = net::UdpSocket::bind(payload.receive_address).expect("Unable to bind to udp receive socket!");
+
+    println!("UDP Receiver thread started for: {}:{}", payload.receive_address.ip(), payload.receive_address.port());
 
     loop {
         // receive loop
@@ -39,24 +112,13 @@ fn receiver_thread(address: SocketAddr, callback: &dyn Fn(&[u8], &UdpSocket) -> 
 
         let buf = &mut buf[..amt];
 
-        // send to queue
-        callback(buf, &sender_socket);
-    }
-}
+        // data receiver, handle
+        let mut packet = GtpV1::init(buf.to_vec(), payload.teid);
+        match payload.send_socket.send(packet.serialize().as_ref()) {
+            Ok(_) => {},
+            Err(e) => {println!("Unable to send gtp downlink packet, {:?}", e)},
+        };
 
-fn udp_callback(data: &[u8], socket: &UdpSocket) -> () {
-    let mut packet = GtpV1::init(data.to_vec());
-    match socket.send(packet.serialize().as_ref()) {
-        Ok(_) => {},
-        Err(e) => {println!("Unable to send gtp downlink packet, {:?}", e)}
-    }
-}
-
-fn gtp_callback(data: &[u8], socket: &UdpSocket) -> () {
-    let packet = GtpV1::from_gtp(data);
-    match socket.send(packet.get_data()) {
-        Ok(_) => {},
-        Err(e) => {println!("Unable to send udp uplink data, {:?}", e)}
     }
 }
 
@@ -65,37 +127,74 @@ fn main() -> std::io::Result<()> {
 
     // parse args
     let args: Vec<String> = env::args().collect();
-    assert!(args.len() >= (3 + 1), "Missing argument. Required <gtp target> <listener> <uplink target>");
+    assert!(args.len() >= (1 + 1), "Missing argument. Required <config path>");
 
-    // --- Senders
-    // address which producer gtp packets are send to
-    let gtp_target = args.get(1).unwrap().parse::<SocketAddr>().unwrap();
+    // Read configuration from json
+    let file = fs::File::open(args.get(1).unwrap())?;
+    let reader = BufReader::new(file);
+    let config: ConfigRoot = serde_json::from_reader(reader)?;
 
-    // address which, data of received gtp packets send to
-    let uplink_udp_send_address = args.get(3).unwrap().parse::<SocketAddr>().unwrap();
+    let gtp_sender = SocketAddr::new(config.gtp.sender.host.parse::<IpAddr>().unwrap(), config.gtp.sender.port.parse::<u16>().unwrap());
+    let gtp_receiver = SocketAddr::new(config.gtp.receiver.host.parse::<IpAddr>().unwrap(), config.gtp.receiver.port.parse::<u16>().unwrap());
+    let gtp_send_socket = net::UdpSocket::bind("0.0.0.0:0").expect("Unable to bind to sender socket");
+    gtp_send_socket.connect(gtp_sender).expect("Unable to connect to GTP sender socket.");
 
-    // --- Listeners
-    // address which incoming udp packets are listened from
-    let udp_listener_address = args.get(2).unwrap().parse::<SocketAddr>().unwrap();
+    let mut tunnels: HashMap<u32, SenderReceiverPortPair> = HashMap::new();
+    let mut receiver_tunnels: HashMap<u32, SocketAddr> = HashMap::new();
 
-    // address of incoming gtp packets listened from
-    let gtp_listen_address = SocketAddr::new(
-        udp_listener_address.ip(),
-        2152
-    );
+    println!("GTP sender: {}:{}", gtp_sender.ip(), gtp_sender.port());
+    println!("GTP receiver: {}:{}", gtp_receiver.ip(), gtp_receiver.port());
+    println!("Tunnels:\n------");
 
-    println!("{:?}",gtp_target);
-    println!("{:?}",uplink_udp_send_address);
-    println!("{:?}",udp_listener_address);
-    println!("{:?}",gtp_listen_address);
+    for (_index, tunnel) in config.tunnels.iter().enumerate() {
+        let udp_receiver = SocketAddr::new(
+            tunnel.connection.receiver.host.parse::<IpAddr>().unwrap(),
+            tunnel.connection.receiver.port.parse::<u16>().unwrap()
+        ); 
+        let udp_sender = SocketAddr::new(
+            tunnel.connection.sender.host.parse::<IpAddr>().unwrap(),
+            tunnel.connection.sender.port.parse::<u16>().unwrap()
+        );
 
-    // start receiver threads
-    let udp_thread = thread::spawn(move || receiver_thread(udp_listener_address, &udp_callback, gtp_target));
-    let gtp_thread = thread::spawn(move || receiver_thread(gtp_listen_address, &gtp_callback, uplink_udp_send_address));
+        let pair = SenderReceiverPortPair{
+            teid: tunnel.teid,
+            sender: udp_sender
+        };
 
-    let _udp_thread: &Thread = udp_thread.thread();
+        tunnels.insert(tunnel.teid, pair);  // move to map
+        let pair = tunnels.get(&tunnel.teid).unwrap();  // get a ref to print info
 
-    udp_thread.join().unwrap();
+        receiver_tunnels.insert(tunnel.teid, udp_receiver);
+        let receiver_pair = receiver_tunnels.get(&tunnel.teid).unwrap();
+
+        println!("{}- udp receive->{}:{}, udp send->{}:{}", 
+            pair.teid,
+            receiver_pair.ip(), 
+            receiver_pair.port(), 
+            pair.sender.ip(), 
+            pair.sender.port()
+        );
+    }
+    println!("-----");
+
+
+    // start gtp receiver thread
+    let gtp_thread = thread::spawn(move || gtp_receiver_thread(GtpReceiverPayload{
+        teid_map: tunnels,
+        receive_address: gtp_receiver
+    }));
+
+    // start udp receiver threads
+    let threads: Vec<JoinHandle<_>> = receiver_tunnels.iter().map(|(&teid, &addr)| {
+        let new_socket = gtp_send_socket.try_clone().unwrap();
+        thread::spawn(move || udp_receiver_thread(UdpReceiverPayload{
+            teid: teid.to_owned(),
+            send_socket: new_socket,
+            receive_address: addr.to_owned()
+        }))
+    }).collect();
+
+    // join everything
     gtp_thread.join().unwrap();
 
     Ok(())
